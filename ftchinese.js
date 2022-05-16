610b515801de736e15d49b4aeab93f5895750c8c
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-05-16 11:23:04 +0800

    Update ftchinese.js

--- ftchinese.js
+++ ftchinese.js
@@ -22,24 +22,4 @@ hostname=user.ftmailbox.cn
 ***********************************/
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$done({body: JSON.stringify({environment:"Production",receipt:{in_app:[{quantity:"1",product_id:"com.ft.ftchinese.mobile.subscription.vip",expires_date:"2023-04-02 22:22:22 Etc/GMT",expires_date_ms:"1680465780000",expires_date_pst:"2023-04-02 22:22:22 America/Los_Angeles",in_app_ownership_type:"PURCHASED"}]},latest_receipt_info:[{quantity:"1",product_id:"com.ft.ftchinese.mobile.subscription.vip",expires_date:"2023-04-02 22:22:22 Etc/GMT",expires_date_ms:"1680465780000",expires_date_pst:"2023-04-02 22:22:22 America/Los_Angeles",in_app_ownership_type:"PURCHASED"}],latest_receipt:"NA",pending_renewal_info:[{auto_renew_product_id:"com.ft.ftchinese.mobile.subscription.vip",product_id:"com.ft.ftchinese.mobile.subscription.vip",auto_renew_status:"0"}],status:0})});
+$done({body: JSON.stringify({environment:"Production",receipt:{in_app:[{quantity:"1",product_id:"com.ft.ftchinese.mobile.subscription.vip",expires_date:"2024-04-02 22:22:22 Etc/GMT",expires_date_ms:"1712028100000",expires_date_pst:"2024-04-02 22:22:22 America/Los_Angeles",in_app_ownership_type:"PURCHASED"}]},latest_receipt_info:[{quantity:"1",product_id:"com.ft.ftchinese.mobile.subscription.vip",expires_date:"2024-04-02 22:22:22 Etc/GMT",expires_date_ms:"1712028100000",expires_date_pst:"2024-04-02 22:22:22 America/Los_Angeles",in_app_ownership_type:"PURCHASED"}],latest_receipt:"NA",pending_renewal_info:[{auto_renew_product_id:"com.ft.ftchinese.mobile.subscription.vip",product_id:"com.ft.ftchinese.mobile.subscription.vip",auto_renew_status:"0"}],status:0})});
