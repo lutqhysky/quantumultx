8f416267688c28bc2f486dbcd06304494637b120
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-06-06 11:33:14 +0800

    Add files via upload

---
+++ peiyinxiu.js
@@ -0,0 +1,25 @@
+/*
+app下载地址：商店搜索：知音漫客
+^https?:\/\/iosapi.peiyinxiu.com url script-response-body peiyinxiu.js
+MITM = *.peiyinxiu.com
+作者：清清情
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = 'api/User';
+
+if (url.indexOf(vip) != -1) {
+    obj.data.is_vip = 1;
+    obj.data.gold = 1;
+    obj.data.vip_end_time = 9999999999000;
+    obj.data.gold2 = 1;
+    obj.data.is_can_apply = 1;
+    body = JSON.stringify(obj);
+    
+}
+
+$done({body});
+
---
+++ seqpy.js
@@ -0,0 +1,29 @@
+/*
+app下载地址：商店搜索：少儿趣配音
+^https?:\/\/childapi40.qupeiyin.com url script-response-body seqpy.js
+MITM = *.qupeiyin.com
+作者：清清情
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/member';
+const vip2 = '/member';
+
+if (url.indexOf(vip) != -1) {
+    obj.data.is_svip = "1";
+    obj.data.is_vip = "1";
+    obj.data.svip_endtime = "9999999999";
+    obj.data.vip_endtime = "9999999999";
+    body = JSON.stringify(obj);
+    
+}
+if (url.indexOf(vip2) != -1) {
+    obj.data.is_vip = "1";
+    body = JSON.stringify(obj);
+    
+}
+$done({body});
+
