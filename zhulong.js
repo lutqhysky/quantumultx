da5742a06c66a0d684ead3e8a821f069a454e268
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-07-27 11:31:05 +0800

    Update zhulong.js

--- zhulong.js
+++ zhulong.js
@@ -1,24 +1,46 @@
 /*
->app下载地址：商店搜索：筑龙学社（现已下载，商店最终版本可用）
->^https?:\/\/(www|m).zhulong\.com url script-response-body zhulong.js
->MITM = *.zhulong.com
->作者：清清情 时间：2022年6月6日
+app下载地址：商店搜索：筑龙
+^https?:\/\/(www|m).zhulong.com\/(ucenter/prod-api/personal/myinfo/getUserInfo|edu/prod-api/lesson/lesson/getScheduleByLessonId|/ucenter/prod-api/personal/History/lessonview|edu/prod-api/lesson/lesson/getLesson|edu/prod-api/lesson/lesson/play|bbs/prod-api/thread/thread/getThreadForTid) url script-response-body zhulong.js
+MITM = *.zhulong.com
+作者：清清情
 */
-
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
-
-const vip = '/ucenter/prod-api/personal/myinfo';
-const buy = 'edu/prod-api/lesson/lesson';
-
+const vip = '/ucenter/prod-api/personal/myinfo/getUserInfo';
+const buy = '/edu/prod-api/lesson/lesson/getScheduleByLessonId';
+const bought = '/ucenter/prod-api/personal/History/lessonview';
+const buybuy = '/edu/prod-api/lesson/lesson/getLesson';
+const play = '/edu/prod-api/lesson/lesson/play';
+const down = '/bbs/prod-api/thread/thread/getThreadForTid';
 if (url.indexOf(vip) != -1) {
     obj.result.is_vip = 1;
     obj.result.vip_level = 1;
-    body = JSON.stringify(obj);
+    body = JSON.stringify(obj);    
 }
 if (url.indexOf(buy) != -1) {
     obj.result.is_buy = 1;
-    body = JSON.stringify(obj);
+    body = JSON.stringify(obj);    
+}
+if (url.indexOf(bought) != -1) {
+    obj.result.list.is_buy = 1;
+    body = JSON.stringify(obj);    
+}
+if (url.indexOf(buybuy) != -1) {
+    obj.result.lesson_info.is_vip = 1;
+    obj.result.is_buy = 1;
+    obj.result.vip_level = 1;
+    body = JSON.stringify(obj);    
+}
+if (url.indexOf(play) != -1) {
+    obj.result.is_vip = 1;
+    obj.result.is_buy = 1;
+    body = JSON.stringify(obj);    
+}
+if (url.indexOf(down) != -1) {
+    obj.result.can_down = 1;
+    body = JSON.stringify(obj);    
 }
 $done({body});
+
+
