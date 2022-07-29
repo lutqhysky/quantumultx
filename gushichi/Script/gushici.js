f15980080053de38dbe0432ce5a2f10f54916924
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-07-29 11:32:22 +0800

    Create gushici.js

---
+++ gushichi/Script/gushici.js
@@ -0,0 +1,19 @@
+/*
+app下载地址：商店搜索：古诗词
+^https?:\/\/gushici.91iread.com url script-response-body gushici.js
+MITM =gushici.91iread.com
+作者：清清情
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/api/user/me';
+
+if (url.indexOf(vip) != -1) {
+    obj.data.isVip = 1;
+    body = JSON.stringify(obj);
+    
+}
+$done({body});
