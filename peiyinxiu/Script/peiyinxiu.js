74ae60fd44f223276cf469f578f64458169e7442
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-02 18:00:10 +0800

    Update peiyinxiu.js

--- peiyinxiu/Script/peiyinxiu.js
+++ peiyinxiu/Script/peiyinxiu.js
@@ -9,13 +9,19 @@ var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-const vip = 'api/User';
-
+const vip = '/Api/user/GetMyInfo';
+const vip2 = '/api/User/GetVipDetail';
 if (url.indexOf(vip) != -1) {
     obj.data.is_vip = 1;
     obj.data.gold = 999999;
     obj.data.vip_end_time = 9876543210000;
     obj.data.gold2 = 999999;
+    obj.data.is_can_apply = 1;
+    body = JSON.stringify(obj);
+}
+if (url.indexOf(vip2) != -1) {
+    obj.data.is_vip = 1;
+    obj.data.vip_end_time = 9876543210000;
     obj.data.is_can_apply = 1;
     body = JSON.stringify(obj);
 }
