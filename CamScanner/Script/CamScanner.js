6c8741546eb95e6958ba9e4ec94785ab1500f9d9
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-03 08:53:39 +0800

    Update CamScanner.js

--- CamScanner/Script/CamScanner.js
+++ CamScanner/Script/CamScanner.js
@@ -3,22 +3,16 @@ CamScanner 解锁部分高级特权
 ***************************
 Quantumult X:
 [rewrite_local]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/CamScanner/Script/CamScanner.js
 [mitm]
 hostname = ap*.intsig.net
 ***************************
 Surge4 or Loon:
 [Script]
-http-response ^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+http-response ^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/CamScanner/Script/CamScanner.js
 [MITM]
 hostname = ap*.intsig.net
 ***************************
-Quantumult:  
-[REWRITE]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
-[MITM]
-hostname = ap*.intsig.net
-**************************/
 let body = JSON.parse($response.body)
     body.data.psnl_vip_property = {"renew_method": "appstore",
       "initial_tm": "1614867690",
