419fd542b43a3c9a8a5d19d7181ecf405e2e3596
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-04 13:35:21 +0800

    Update peiyinxiu.js

--- peiyinxiu/Script/peiyinxiu.js
+++ peiyinxiu/Script/peiyinxiu.js
@@ -14,14 +14,14 @@ const vip2 = '/api/User/GetVipDetail';
 if (url.indexOf(vip) != -1) {
     obj.data.is_vip = 1;
     obj.data.gold = 999999;
-    obj.data.vip_end_time = 2099-12-12;
+    obj.data.vip_end_time = 9876543210;
     obj.data.gold2 = 999999;
     obj.data.is_can_apply = 1;
     body = JSON.stringify(obj);
 }
 if (url.indexOf(vip2) != -1) {
     obj.data.is_vip = 1;
-    obj.data.vip_end_time = 2099-12-12;
+    obj.data.vip_end_time = 9876543210;
     obj.data.is_can_apply = 1;
     body = JSON.stringify(obj);
 }
