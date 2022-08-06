5786c0d56aefaefb4ad67b84c5c7c9abceef9693
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-06 15:09:00 +0800

    Update Readmoo.js

--- Readmoo/Script/Readmoo.js
+++ Readmoo/Script/Readmoo.js
@@ -1,16 +1,13 @@
 /* 
  app下载地址：商店搜索：Readmoo 时间 ：2022-08-01 
- ^https?:\/\/api.readmoo.com url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/Readmoo/Script/Readmoo.js
+ ^https?:\/\/api.readmoo.com/store/v3/books/210000175000101/reviews url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/Readmoo/Script/Readmoo.js
  MITM = api.readmoo.com
  作者：清清情 
  */ 
-var body = $response.body; 
-var url = $request.url; 
-var obj = JSON.parse(body); 
-const buy = '/store/v3/books/210000175000101/reviews'; 
- 
-if (url.indexOf(buy) != -1) { 
-      obj.included.attributes.subscribable = true; 
-      body = JSON.stringify(obj);  
-} 
+
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj.subscribable = true;
+body = JSON.stringify(obj);
 $done({body}); 
