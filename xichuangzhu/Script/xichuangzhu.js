6a66848dbc63c099fa037d527bbbb9b62dcf4839
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-08 17:32:38 +0800

    Update xichuangzhu.js

--- xichuangzhu/Script/xichuangzhu.js
+++ xichuangzhu/Script/xichuangzhu.js
@@ -1,6 +1,6 @@
 /* 
  app下载地址：商店搜索：西窗烛 时间 ：2022-08-08 
- ^https?:\/\/lchttpapi.xczim.com\/1.1\/(users|functions/checkDidBoughtBook|functions/receiveBookCollectionByProUser|functions/getMyCoins) url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/xichuangzhu/Script/xichuangzhu.js
+ ^https?:\/\/lchttpapi.xczim.com\/1.1\/(users|functions/checkDidBoughtBook|functions/receiveBookCollectionByProUser|functions/getMyCoins|functions/buyFont) url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/xichuangzhu/Script/xichuangzhu.js
  MITM = *.xczim.com
  作者：清清情 
  */ 
@@ -13,6 +13,7 @@
  const buy = '/1.1/functions/checkDidBoughtBook'; 
  const bought = '/1.1/functions/receiveBookCollectionByProUser'; 
  const Coins = '/1.1/functions/getMyCoins'; 
+ const Font = '/1.1/functions/buyFont'; 
  if (url.indexOf(vip) != -1) { 
     obj.membership = true; 
     obj.premiumMembership = true; 
@@ -34,6 +35,11 @@
     obj.result.androidCoins = 10000;
     obj.result.money = 10000;
     obj.result.iosCoins = 10000;
+    body = JSON.stringify(obj);  
+ } 
+  if (url.indexOf(Font) != -1) { 
+    obj.code = 0;
+    obj.error = null;
     body = JSON.stringify(obj);  
  } 
 $done({body}); 
