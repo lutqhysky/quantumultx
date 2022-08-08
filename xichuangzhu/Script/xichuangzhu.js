079a37acd84f9b7ec3e69b2ee12545e279dc65ce
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-08 14:53:33 +0800

    Update xichuangzhu.js

--- xichuangzhu/Script/xichuangzhu.js
+++ xichuangzhu/Script/xichuangzhu.js
@@ -4,12 +4,15 @@
  MITM = *.xczim.com
  作者：清清情 
  */ 
+
  var body = $response.body; 
  var url = $request.url; 
  var obj = JSON.parse(body); 
+
  const vip = '/1.1/users'; 
  const buy = '/1.1/functions/checkDidBoughtBook'; 
  const bought = /1.1/functions/receiveBookCollectionByProUser'; 
+
  if (url.indexOf(vip) != -1) { 
     obj.membership = true; 
     obj.premiumMembership = true; 
