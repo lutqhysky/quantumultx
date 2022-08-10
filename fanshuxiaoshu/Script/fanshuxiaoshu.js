bd2396e8664249d91cfc6a8fd0a6d3f742befbeb
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-10 09:28:07 +0800

    Update fanshuxiaoshu.js

--- fanshuxiaoshu/Script/fanshuxiaoshu.js
+++ fanshuxiaoshu/Script/fanshuxiaoshu.js
@@ -1,6 +1,6 @@
 /*
 app下载地址：商店搜索：番薯小说
-^https?:\/\/ggs\.manmeng168\.com\/v1\/user\/info url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/fanshuxiaoshu/Script/fanshuxiaoshu.js
+^https?:\/\/ggs\.manmeng168\.com\/v1\/(user\/info|ios/user/info) url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/fanshuxiaoshu/Script/fanshuxiaoshu.js
 MITM = ggs.manmeng168.com
 作者：清清情
 */
@@ -10,10 +10,15 @@ var url = $request.url;
 var obj = JSON.parse(body);
 
 const vip = '/v1/user/info?';
-
+const buy = '/v1/ios/user/info?';
 if (url.indexOf(vip) != -1) {
     obj.data.vip = true;
-    obj.data.vip_end_time = 9876543210;
+    obj.data.vip_end_time = 20290427130123;
+    body = JSON.stringify(obj);
+}
+if (url.indexOf(buy) != -1) {
+    obj.data.userInfo.vip = true;
+    obj.data.userInf.vip_end_time = 20290427130123;
     body = JSON.stringify(obj);
 }
 $done({body});
