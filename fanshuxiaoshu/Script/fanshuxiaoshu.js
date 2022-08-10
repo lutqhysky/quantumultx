e39f69ace359618ebf05177c55182815e9b0341f
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-10 09:15:37 +0800

    Create fanshuxiaoshu.js

---
+++ fanshuxiaoshu/Script/fanshuxiaoshu.js
@@ -0,0 +1,19 @@
+/*
+app下载地址：商店搜索：番薯小说
+^https?:\/\/ggs\.manmeng168\.com\/v1\/user\/info url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/gushichi/Script/gushici.js
+MITM =gushici.91iread.com
+作者：清清情
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/v1/user/info?';
+
+if (url.indexOf(vip) != -1) {
+    obj.data.vip = true;
+    obj.data.vip_end_time = 9876543210;
+    body = JSON.stringify(obj);
+}
+$done({body});
