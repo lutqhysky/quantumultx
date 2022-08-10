72c6d293f5477c2d4dd049e3e3dcf59ad7f99fd4
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-10 15:14:14 +0800

    Update gushici.js

--- gushichi/Script/gushici.js
+++ gushichi/Script/gushici.js
@@ -1,6 +1,6 @@
 /*
 app下载地址：商店搜索：古诗词
-^https?:\/\/gushici.91iread.com:8011/api\/(user\/me|iap/verifyorder) url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/gushichi/Script/gushici.js
+^https?:\/\/gushici\.91iread.com:8011\/api\/(user\/me|iap\/verifyorder) url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/gushichi/Script/gushici.js
 MITM =gushici.91iread.com
 作者：清清情
 */
@@ -13,7 +13,6 @@ const vip = '/api/user/me';
 const buy = '/iap/verifyorder';
 if (url.indexOf(vip) != -1) {
     obj.data.isVip = 1;
-    obj.data.vip_end_time = 9876543210;
     body = JSON.stringify(obj);
 }
 if (url.indexOf(buy) != -1) {
