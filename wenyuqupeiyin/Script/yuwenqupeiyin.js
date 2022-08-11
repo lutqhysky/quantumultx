574f9a6d122ba39187565fbddeda32937a4c32f1
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-11 08:42:38 +0800

    Update yuwenqupeiyin.js

--- wenyuqupeiyin/Script/yuwenqupeiyin.js
+++ wenyuqupeiyin/Script/yuwenqupeiyin.js
@@ -1,6 +1,6 @@
 /*
 app下载地址：商店搜索：语文趣配音
-^https?:\/\/cnapi\.qupeiyin\.com\/(user\/memberData|member) url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/wenyuqupeiyin/Script/yuwenqupeiyin.js
+^https?:\/\/cnapi\.qupeiyin\.com\/(user\/memberData|member|top/show\Top) url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/wenyuqupeiyin/Script/yuwenqupeiyin.js
 MITM = *.qupeiyin.com
 作者：清清情  时间2022年6月6日
 */
@@ -11,7 +11,7 @@ var obj = JSON.parse(body);
 
 const vip = '/user/memberData';
 const svip = '/member';
-
+const svip = '/top/showTop';
 if (url.indexOf(vip) != -1) {
     obj.data.is_svip = "1";
     obj.data.is_vip = "1";
@@ -27,4 +27,13 @@ if (url.indexOf(svip) != -1) {
     obj.data.vip_endtime = "999999999";
     body = JSON.stringify(obj);
  }
+if (url.indexOf(svip) != -1) {
+    obj.data.top_info.is_svip = "1";
+    obj.data.top_info.is_vip = "1";
+    body = JSON.stringify(obj);
+ }
 $done({body});
+
+
+
+
