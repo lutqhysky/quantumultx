86f5a44bae5e500a8e82fe949e24beec69478a7c
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-11 18:05:49 +0800

    Update yunting.js

--- yunting/Script/yunting.js
+++ yunting/Script/yunting.js
@@ -1,25 +1,7 @@
-/* 
- app下载地址：商店搜索：云听 时间 ：2022-08-01 
- ^https?:\/\/(39\.106\.98\.80|ytapi\.radio\.cn)\/(app2355d832116cc8bd\/2b31294abeefdf0bfc305dd63fd052c5\/9fd11b27cf4382ad25557f49496f7032|ytsrv\/srv\/appUser\/getUserInfoH5) url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/yunting/Script/yunting.js
- MITM = 39.106.98.80, *.radio.cn
- 作者：清清情 ,解锁VIP，但是下面有些碍眼的东西，没去掉。。
- */ 
-var body = $response.body; 
-var url = $request.url; 
-var obj = JSON.parse(body); 
-const vip = '/app2355d832116cc8bd/2b31294abeefdf0bfc305dd63fd052c5/9fd11b27cf4382ad25557f49496f7032'; 
-const buy = '/ytsrv/srv/appUser/getUserInfoH5'; 
-if (url.indexOf(vip) != -1) { 
-       obj.isPurchased = 1;
-       obj.isSubscribe = 1;
-       obj.isVip = 1;
-       body = JSON.stringify(obj);  
- } 
-if (url.indexOf(buy) != -1) { 
-       obj.object.baseInfo.isVip = 1;
-       obj.object.baseInfo.vipTime = "2029-12-12";
-       obj.object.baseInfo.user_type = 6;
-       obj.object.baseInfo.iconStatus = 99999;
-       body = JSON.stringify(obj);  
- } 
-$done({body}); 
+/*
+Unlocks by Cuttlefish 公众号：墨鱼手记
+
+ulr 匹配路径 http://getway.radio.cn/app*
+*/
+var body = $response.body.replace(/\u0069\u0073\u0056\u0069\u0070\u0022\u003A\u0031/g, '\u0069\u0073\u0056\u0069\u0070\u0022\u003A\u0030');
+$done({ body });
