50e10f67b1ab128d66f0b772ae38561f07d60745
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-11 14:37:26 +0800

    Update yilakanshu.js

--- yilakanshu/Script/yilakanshu.js
+++ yilakanshu/Script/yilakanshu.js
@@ -1,22 +1,27 @@
-/*
-app下载地址：商店搜索：咿啦看书
-^https?:\/\/bookapi\.ellabook\.cn\/rest\/api\/service url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/yilakanshu/Script/yilakanshu.js
-MITM = bookapi.ellabook.cn
-作者：清清情  时间2022年6月6日
-*/
+/***********************************
 
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
+> 應用名稱：咿啦看書
+> 軟件版本：2.8.5
+> 下載地址：https://apps.apple.com/cn/app/id1176775033
+> 腳本作者：Cuttlefish
+> 微信賬號：墨魚手記
+> 更新時間：2022-02-17
+> 通知頻道：https://t.me/ddgksf2021
+> 問題反饋：https://t.me/ddgksf2013_bot
+> 特别說明：本腳本僅供學習交流使用，禁止轉載售賣
+ 
+[rewrite_local]
 
-const vip = '/rest/api/service';
+# ～ 咿啦看書解鎖會員權限（2022-02-17）@ddgksf2013
+https?:\/\/bookapi\.ellabook\.cn\/rest\/api\/service$ url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/elabook.js
 
-if (url.indexOf(vip) != -1) {
-    obj.data.userInfo.userInfo.isLbVip = "YES";
-    obj.data.userInfo.userInfo.is_svip = "YES";
-    obj.data.userInfo.userInfo.is_vip = "YES";
-    obj.data.userInfo.userInfo.vip_end_date = 1;
-    obj.data.englishUser.isVip = "Y";
-    body = JSON.stringify(obj);  
-}
+[mitm] 
+
+hostname=bookapi.ellabook.cn
+
+***********************************/
+body = $response.body.replace(/ip":"NO"/g, "ip\":\"YES\"")
+                     .replace(/ip":"VIP_NO"/g, "ip\":\"VIP_YES\"")
+                     .replace(/code":"\d{10}/g, "code\":\"0x00000000")
+                     .replace(/status":"\d/g, "status\":\"1");
 $done({body});
