21e58348c6afdcfd744482db88225d74ee9af437
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-12 16:10:54 +0800

    Update Readmoo.js

--- Readmoo/Script/Readmoo.js
+++ Readmoo/Script/Readmoo.js
@@ -1,10 +1,11 @@
 /* 
  app下载地址：商店搜索：Readmoo 时间 ：2022-08-01 
   MITM = api.readmoo.com
+  ^https?:\/\/api\.readmoo\.com\/store\/v3\/books\/210240108000101\/reviews url response-body subscribable":false response-body subscribable":true
  作者：清清情 
  */ 
 
-^https?:\/\/api\.readmoo\.com\/store\/v3\/books\/210240108000101\/reviews url response-body subscribable":false response-body subscribable":true
+
 
 body = $response.body.replace(/subscribable":false/g, "subscribable\":\"true\"")
 $done({body});
