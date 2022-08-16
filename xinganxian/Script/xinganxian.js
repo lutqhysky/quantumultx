fc8deef3b7eab0a59ff4850ea4f7421b38e49af1
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-16 11:30:54 +0800

    Update xinganxian.js

--- xinganxian/Script/xinganxian.js
+++ xinganxian/Script/xinganxian.js
@@ -1,13 +1,13 @@
 /* 
  app下载地址：商店搜索：新干线 时间 ：2022-08-16 
- ^https?:\/\/api\.fwxgx\.com\/aggregation\/v1\/courses/* url script-response-body zhulong.js 
+ ^https?:\/\/api\.fwxgx\.com\/aggregation\/v1\/courses url script-response-body zhulong.js 
  MITM = api.fwxgx.com
  作者：清清情 
  */ 
  var body = $response.body; 
  var url = $request.url; 
  var obj = JSON.parse(body); 
- const buy = '/aggregation/v1/courses/*'; 
+ const buy = '/aggregation/v1/courses'; 
 
 if (url.indexOf(buy) != -1) { 
     obj.bought = true; 
