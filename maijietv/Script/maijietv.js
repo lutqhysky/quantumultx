722edbd4d843a93c5859f0f272b9721817d7d89b
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-19 08:30:08 +0800

    Update maijietv.js

--- maijietv/Script/maijietv.js
+++ maijietv/Script/maijietv.js
@@ -1,6 +1,6 @@
 /*
 app下载地址：麦秸TV
-^https?:\/\/api\.maijitv\.com\/(mes|urm)\/(mesIosRechargeList|queryMesUserInfo|getUserInfo) url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/maijietv/Script/maijietv.js
+^https?:\/\/api\.maijitv\.com\/(mes|urm|common)\/(mesIosRechargeList|queryMesUserInfo|getUserInfo|queryKnowledgeResItems) url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/maijietv/Script/maijietv.js
 MITM =api.maijitv.com
 作者：清清情
 */
@@ -12,6 +12,7 @@ var obj = JSON.parse(body);
 const vip = '/mes/mesIosRechargeList';
 const buy = '/mes/queryMesUserInfo';
 const bought = '/urm/getUserInfo';
+const jiesuo = '/common/queryKnowledgeResItems';
 
 if (url.indexOf(vip) != -1) {
      obj.body.expireTime = 9876543210;
@@ -25,6 +26,10 @@ if (url.indexOf(vip) != -1) {
 }
 if (url.indexOf(bought) != -1) {
      obj.body.status = 1;
+     body = JSON.stringify(obj);    
+}
+if (url.indexOf(bought) != -1) {
+     obj.body.resItems.freeScope = "FREE";
      body = JSON.stringify(obj);    
 }
 $done({body});
