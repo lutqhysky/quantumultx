5a870adae6bfd5c5582280ed79fe170475ee6146
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-23 17:14:45 +0800

    Update Vister.js

--- Vister/Script/Vister.js
+++ Vister/Script/Vister.js
@@ -13,6 +13,6 @@
 
 
 
-body = $response.body.replace(/isVip":0/g, 'isVip":1').replace(/endTime":\d{13}/g, 'endTime":2652198399000').replace(/isBuyMag":0/g, 'isBuyMag":1').replace(/isBuyArticle":0/g, 'isBuyArticle":1');
+body = $response.body.replace(/expireVip":1/g, 'iexpireVip":0').replace(/isFree":0/g, 'isFree":1').replace(/isMiniVip":0/g, 'isMiniVip":1').replace(/isActive":0/g, 'isActive":1').replace(/isVip":0/g, 'isVip":1').replace(/endTime":\d{13}/g, 'endTime":2652198399000').replace(/isBuyMag":0/g, 'isBuyMag":1').replace(/isBuyArticle":0/g, 'isBuyArticle":1');
 $done({body});
 
