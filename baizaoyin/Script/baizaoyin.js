8bb0daf91f747dd49cbecefeefb5fbc0b950420e
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-23 11:05:14 +0800

    Update baizaoyin.js

--- baizaoyin/Script/baizaoyin.js
+++ baizaoyin/Script/baizaoyin.js
@@ -3,7 +3,7 @@
   MITM = matrix.fingerplay.cn
  ^http:\/\/matrix\.fingerplay\.cn\/user\/fetchUserInfo url response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/baizaoyin/Script/baizaoyin.js
  作者：清清情
- 下面，不用一级一级的去找了，直接像网球锤子，替换就可以了。。。！！！！，非常方便。不过这个还是不能用，需要修改的数据不止这一个。。继续努力。。
+ 下面，不用一级一级的去找了，直接像网球锤子，替换就可以了。。。！！！！
  */ 
 
 
