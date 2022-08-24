109debd8f9ef8242403ab60e3b30d306f5fd4cb2
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-24 12:00:05 +0800

    Update shaoniandedao.js

--- shaomiandedao/Script/shaoniandedao.js
+++ shaomiandedao/Script/shaoniandedao.js
@@ -4,6 +4,9 @@
  ^https?:\/\/igetcool-gateway.igetcool\.com\/（app-api-user-server\/oath\/user\/info\.json|app-api-product-server\/option\/course\/audios\.json|app-api-product-server\/white\/course\/videos\.json） url response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/Vister/Script/Vister.js
  作者：清清情
  下面，不用一级一级的去找了，直接像网球锤子，替换就可以了。。。！！！！
+ https://igetcool-gateway.igetcool.com/app-api-user-server/oath/user/info.json
+ https://igetcool-gateway.igetcool.com/app-api-product-server/option/course/audios.json
+ https://igetcool-gateway.igetcool.com/app-api-product-server/white/course/videos.json
  */ 
 body = $response.body.replace(/hasPay":0/g, 'hasPay":1').replace(/userTag":0/g, 'userTag":1').replace(/audioActivityLockStatus":\d{1}/g, 'audioActivityLockStatus":2').replace(/buy":0/g, 'buy":1').replace(/free":0/g, 'free":1');
 $done({body});
