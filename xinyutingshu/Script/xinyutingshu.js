9cfa0dcc508da901f53a9d9e46bd020110ba47c3
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-24 14:21:51 +0800

    Create xinyutingshu.js

---
+++ xinyutingshu/Script/xinyutingshu.js
@@ -0,0 +1,18 @@
+/*
+app下载地址：心语听书
+^https?:\/\/i\.xinyulib\.com\.cn\/api\/querytoken url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/Libvio/Script/Libvio.jsMITM =apjson.nftvio.space
+MITM =i.xinyulib.com.cn
+作者：清清情
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/api/querytoken';
+
+if (url.indexOf(vip) != -1) {
+    obj.data.vipendtime = "2029-05-31";   
+    body = JSON.stringify(obj);    
+}
+$done({body});
