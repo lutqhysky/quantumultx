e6dc98ceb686d82bfd5a48246a7e3770cbb87a7e
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-25 18:03:48 +0800

    Update xiaozhitiao.js

--- xiaozhitiao/Script/xiaozhitiao.js
+++ xiaozhitiao/Script/xiaozhitiao.js
@@ -1,6 +1,6 @@
 /*
 >app下载地址：商店搜索：纸条
->^https?:\/\/(app1|zwzt-h5)\.zuowenzhitiao.com\/(user|content|behavior|longApi)\/(common|member|seminar|content|article)\/(getPersonInfo|info|getSeminarDetailNoContentList|list|openInfo|article|commonInfo)\/content\/detail url script-response-body xiaozhitiao.js
+>^https?:\/\/(app1|zwzt-h5)\.zuowenzhitiao.com url script-response-body xiaozhitiao.js
 >MITM = *.zuowenzhitiao.com
 >作者：清清情 时间：2022年6月6日
 */
@@ -37,20 +37,6 @@ if (url.indexOf(vip3) != -1) {
 }
 if (url.indexOf(vip4) != -1) {
     obj.data.purchased = true;
-    body = JSON.stringify(obj);
-}
-if (url.indexOf(vip5) != -1) {
-    obj.data.member.type = 3;
-    obj.data.member.status = 1;
-    obj.data.member.endTime = 2705680000000;
-    body = JSON.stringify(obj);
-}
-if (url.indexOf(vip6) != -1) {
-    obj.data.activityType = 1;
-    body = JSON.stringify(obj);
-}
-if (url.indexOf(vip7) != -1) {
-    obj.data.seminarList.payLockStatus = 0;
     body = JSON.stringify(obj);
 }
 $done({body});
