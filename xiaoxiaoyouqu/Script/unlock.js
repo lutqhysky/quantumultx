cf5cfd0285f0bee477015e5c403859e7bb52ca91
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-27 11:00:38 +0800

    Update unlock.js

--- xiaoxiaoyouqu/Script/unlock.js
+++ xiaoxiaoyouqu/Script/unlock.js
@@ -5,5 +5,5 @@
 
 
 var body = $response.body.replace(/purchased":false/g, 'subscribable":true')
-                         .replace(/purchased":false/g, 'subscribable":true')
+                         .replace(/purchased":false/g, 'subscribable":true');
 $done({body});
