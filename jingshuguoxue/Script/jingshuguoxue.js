f621830b62713fc3cb59384f187481078e24d22f
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-08-29 14:17:09 +0800

    Create jingshuguoxue.js

---
+++ jingshuguoxue/Script/jingshuguoxue.js
@@ -0,0 +1,9 @@
+/* 
+ app下载地址：Vister看天下
+  MITM = 47.104.228.152
+ ^https?:\/\/47\.104\.228\.152\/api\/apiAppUser\/getUser url response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/Vister/Script/Vister.js
+ 作者：清清情
+ 下面，不用一级一级的去找了，直接像网球锤子，替换就可以了。。。！！！！
+ */ 
+body = $response.body.replace(/cardType":"-\d"/g, 'cardType":"1"').replace(/days":"\d+"/g, 'days":"6666"').replace(/isExpire":"1"/g, 'isExpire":"0"').replace(/expireTime":"."*?"/g, 'expireTime":"2099-05-10"');
+$done({body});
