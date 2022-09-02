476b9584774376935522f55c1c674d57045c072d
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-09-02 22:54:40 +0800

    Update soutu.js

--- sout/Script/soutu.js
+++ sout/Script/soutu.js
@@ -1,2 +1,2 @@
-var body = $response.body.replace(/vip_end":".*?"/g, 'vip_end":"2099/12/12"').replace(/nickname":".*?"/g, 'nickname":"清清情"');
+var body = $response.body.replace(/vip_end":".*?"/g, 'vip_end":"2099/12/12"').replace(/nickname":".*?"/g, 'nickname":"清清情"').replace(/mission_sign_in":\w+/g, 'mission_sign_in":true');
 $done({body});
