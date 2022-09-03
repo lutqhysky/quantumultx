7a7306255fb38657509fcda6d3a237657d34e063
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-09-03 09:50:06 +0800

    Update taotu.js

--- taotu/Script/taotu.js
+++ taotu/Script/taotu.js
@@ -1,2 +1,2 @@
-var body = $response.body.replace(/vip_level":\d+/g, 'vip_level":1').replace(/picSource":\d+/g, 'picSource":1');
+var body = $response.body.replace(/vip_level":\d+/g, 'vip_level":1').replace(/picSource":\d+/g, 'picSource":1') .replace(/hd":\w+/g, 'hd":true');
 $done({body});
