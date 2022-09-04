fb14a867eb1ff9137480501a038486acf8cd70e8
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-09-04 18:49:57 +0800

    Create hyxj.js

---
+++ hyxj/Script/hyxj.js
@@ -0,0 +1,22 @@
+/*
+黄油相机 2022-9-4
+
+^https:\/\/api4\.bybutter\.com\/v4\/users* url script-response-body hhyyxj.js
+
+hostname：api4.bybutter.com
+
+*/
+
+const path1 = "/v4/users";
+let obj = JSON.parse($response.body);
+if ($request.url.indexOf(path1) != -1){
+obj.memberships = [{
+    "endAt": 1977890679,
+    "id": "1",
+    "name": "普通会员",
+    "ownership": "temporary",
+    "startAt": 1662268511,
+    "usageType": "unlimited"
+  }]
+}
+$done({body: JSON.stringify(obj)});
