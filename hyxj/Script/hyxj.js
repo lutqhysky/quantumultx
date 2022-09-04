68a727bbbd413104097d972bea3dd730f60d147f
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-09-04 22:24:53 +0800

    Update hyxj.js

--- hyxj/Script/hyxj.js
+++ hyxj/Script/hyxj.js
@@ -1,13 +1,13 @@
 /*
 黄油相机 2022-9-4
 
-^https:\/\/api4\.bybutter\.com\/v4\/users* url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/hyxj/Script/hyxj.js
+^https:\/\/api4\.bybutter\.com\/v4\/(users|shop) url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/hyxj/Script/hyxj.js
 
 hostname：api4.bybutter.com
 
 */
 
-const path1 = "/v4/users";
+const path1 = "/v4/(users|shop)";
 let obj = JSON.parse($response.body);
 if ($request.url.indexOf(path1) != -1){
 obj.memberships = [{
