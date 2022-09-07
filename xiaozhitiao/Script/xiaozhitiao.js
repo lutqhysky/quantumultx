07a390425e3b0f39aaf353b2da5a4410b7674175
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-09-07 14:15:26 +0800

    Update xiaozhitiao.js

--- xiaozhitiao/Script/xiaozhitiao.js
+++ xiaozhitiao/Script/xiaozhitiao.js
@@ -3,6 +3,7 @@
 >^https?:\/\/(app1|zwzt-h5)\.zuowenzhitiao.com\/(user|content|behavior|longApi)\/(common|member|seminar|content|article)\/(getPersonInfo|info|getSeminarDetailNoContentList|list|openInfo|article|commonInfo) url script-response-body xiaozhitiao.js
 >MITM = *.zuowenzhitiao.com
 >作者：清清情 时间：2022年6月6日
+https://app1.zuowenzhitiao.com/user/extradata
 */
 
 var body = $response.body;
@@ -14,7 +15,9 @@ const vip2 = '/user/member/info';
 const vip3 = '/content/seminar/getSeminarDetailNoContentList';
 const vip4 = '/behavior/paragraph/list';
 const vip5 = '/user/member/openInfo';
-const vip6 = 'longApi/content/article/content/detail';
+const vip6 = '/longApi/content/article/content/detail';
+const vip7 = '/user/extradata';
+
 if (url.indexOf(vip) != -1) {
     obj.data.member.type = 3;
     obj.data.member.endTime = 2705680000000;
@@ -46,4 +49,10 @@ if (url.indexOf(vip6) != -1) {
     obj.data.activityType = 1;
     body = JSON.stringify(obj);
 }
+if (url.indexOf(vip7) != -1) {
+    obj.data.member.type = 3;
+    obj.data.member.endTime = 2705680000000;
+    obj.data.member.status = 1;
+    obj.data.checkStatus = true;
+    body = JSON.stringify(obj);
 $done({body});
