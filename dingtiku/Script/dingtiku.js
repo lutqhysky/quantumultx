1b9b822e73fd0439ed0e6a000ab5f2098e450ec2
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-09-17 15:06:42 +0800

    Update dingtiku.js

--- dingtiku/Script/dingtiku.js
+++ dingtiku/Script/dingtiku.js
@@ -1,2 +1,3 @@
-var body = $response.body.replace(/isBuy":\d+/g, 'isBuy":1').replace(/allowTry":".*?"/g, 'allowTry":"是"');
+var body = $response.body.replace(/isBuy":\d+/g, 'isBuy":1').replace(/allowTry":".*?"/g, 'allowTry":"是"').replace(/code":"\d+"/g, 'code":"00"');
 $done({body});
+
