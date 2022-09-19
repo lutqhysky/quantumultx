3e30615ef081c6fa37ad7487e2369b6889772c5a
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-09-19 15:41:46 +0800

    Update ylttx.js

--- yilutingtianxia/Script/ylttx.js
+++ yilutingtianxia/Script/ylttx.js
@@ -1,2 +1,2 @@
-var body = $response.body.replace(/isVip":\d+/g, 'isVip":0').replace(/isCanListen":\d+/g, 'isCanListen":1').replace(/isFree":\d+/g, 'isFree":1').replace(/isBuy":\d+/g, 'isBuy":1');
+var body = $response.body.replace(/isSubscribe":\d+/g, 'isSubscribe":1').replace(/isVip":\d+/g, 'isVip":0').replace(/isCanListen":\d+/g, 'isCanListen":1').replace(/isFree":\d+/g, 'isFree":1').replace(/isBuy":\d+/g, 'isBuy":1');
 $done({body});
