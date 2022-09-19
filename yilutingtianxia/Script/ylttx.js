dfe4dcba10ce479e8daba238e8d08dd9939d97d6
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-09-19 14:31:20 +0800

    Update ylttx.js

--- yilutingtianxia/Script/ylttx.js
+++ yilutingtianxia/Script/ylttx.js
@@ -1,1 +1,2 @@
-comming soon...
+var body = $response.body.replace(/vipEndTime":\d+/g, 'vipEndTime":9876543210').replace(/vipEndDate":.*?/g, 'vipEndDate":2099-12-12').replace(/vipType":"\w+"/g, 'vipType":"ylttxsvipyear"').replace(/purchaseCount":\d+/g, 'purchaseCount":9876543210').replace(/accountType":"\w+"/g, 'accountType":"ylttxsvipyear"');
+$done({body});
