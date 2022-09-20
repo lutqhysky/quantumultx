e2cbb974e5701f747eef66a961beab8351b3ed0b
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-09-20 23:58:35 +0800

    Update ylttxbuy.js

--- yilutingtianxia/Script/ylttxbuy.js
+++ yilutingtianxia/Script/ylttxbuy.js
@@ -1,2 +1,2 @@
-var body = $response.body.replace(/isBuy":\d+/g, 'isBuy":1').replace(/isCanListen":\d/g, 'isCanListen":1');
+var body = $response.body.replace(/isSubscribe":"\d+"/g, 'isSubscribe":"1"').replace(/isBuy":\d+/g, 'isBuy":1').replace(/isCanListen":\d/g, 'isCanListen":1');
 $done({body});
