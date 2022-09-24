a7df52416fde0235a8b85ef981112a1c12f61656
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-09-24 08:31:40 +0800

    Create ydcd.js

---
+++ ydcd/Script/ydcd.js
@@ -0,0 +1,26 @@
+{
+  "vip": true,
+  "svip": false,
+  "expire": 4093844524891,
+  "permissions": [
+    "ONE_MONTH",
+    "AUTO_SUBSCRIPTION"
+  ],
+  "tag": "TAG_RENEW_EXPECTED",
+  "auto": true,
+  "open": true,
+  "planIds": [
+    "22"
+  ],
+  "svipExpire": 0,
+  "purchasedPlanIds": [
+    "22"
+  ],
+  "firstAuto": false,
+  "copyWritings": [
+    "彭于晏解锁",
+    "QQ群779392027"
+  ]
+}
+
+转载
