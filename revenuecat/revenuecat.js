772ec7f2e0dfa8fa5a3c02c71557c4f075196907
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-10-12 15:56:25 +0800

    Update revenuecat.js

--- revenuecat/revenuecat.js
+++ revenuecat/revenuecat.js
@@ -2,6 +2,9 @@
 #^https?:\/\/api\.revenuecat\.com/v\d/subscribers/\$RCAnonymousID\%\w+ url echo-response text/json echo-response https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/Fileball/Script/Fileball.js
 #mimt: api.revenuecat.com
 #########################################
+var head = $request.headers;
+var ua = head['User-Agent'];
+
 if (ua.indexOf('Fileball') != -1) {
     Body = {"request_date":"1983-03-15T00:00:00Z","request_date_ms":416505600000,"subscriber":{"entitlements":{"filebox_pro":{"expires_date":null,"grace_period_expires_date":null,"product_identifier":"filebox_pro","purchase_date":"1983-03-15T00:00:00Z"}},"first_seen":"1983-03-15T00:00:00Z","last_seen":"1983-03-15T00:00:00Z","management_url":null,"non_subscriptions":{"filebox_pro":[{"id":"yingzi","is_sandbox":false,"original_purchase_date":"1983-03-15T00:00:00Z","purchase_date":"1983-03-15T00:00:00Z","store":"app_store"}]},"original_app_user_id":"$RCAnonymousID%3A8e5a11e56b4246f2ab2b17058c01db1e","original_application_version":"170","original_purchase_date":"1983-03-15T00:00:00Z","other_purchases":{"filebox_pro":{"purchase_date":"1983-03-15T00:00:00Z"}},"subscriptions":{}}};
 }
