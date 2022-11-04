281bcf302f3d77369182ced108c6fe94bdc9c08f
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-11-04 11:22:57 +0800

    Update Pro.js

--- niegou/script/Pro.js
+++ niegou/script/Pro.js
@@ -3,7 +3,7 @@
 2、项目名称：ProKnockOut 解锁订阅
 ************************************
 var head = $request.headers;
-var ua = head['user-agent'];
+var ua = head['User-Agent'];
 
 if (ua.indexOf('%E6%98%9F%E7%A9%BA') != -1) {
     Body = 
