f0646163710c34d7660f4b0d89b8364069b8368d
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-11-10 10:54:01 +0800

    Update yunting.js

--- yunting/Script/yunting.js
+++ yunting/Script/yunting.js
@@ -1,7 +1,31 @@
-/*
-Unlocks by 清清情
+/****************************************
 
-ulr 匹配路径 http://getway.radio.cn/app*
-*/
-var body = $response.body.replace(/isVip":1/g, 'isVip":0').replace(/expire":\d+/g, 'expire":0').replace(/isPurchased":\w+/g, 'isPurchased":1').replace(/needPay":\w+/g, 'needPay":0').replace(/isSubscribe":\w+/g, 'isSubscribe":1').replace(/needPay":\w+/g, 'needPay":0');
-$done({ body });
+项目功能：云听 解锁VIP功能
+下载地址：https://t.cn/A6ouQ21g
+使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+使用方法：需要用时先打开脚本，再打开App。
+
+*****************************************
+
+[rewrite_local]
+
+^http:\/\/getway\.radio\.cn url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/Rewrite/yunting.js
+
+^https:\/\/ytapi\.radio\.cn url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/Rewrite/yunting.js
+
+
+[mitm] 
+
+hostname = *.radio.cn
+
+****************************************/
+
+
+const getway = "/getway.radio.cn";
+const ytapi = "/ytapi.radio.cn";
+
+if ($request.url.indexOf(getway) != -1){body = $response.body.replace(/\"isVip":\d+/g, '\"isVip":0').replace(/\"needPay":\d+/g, '\"needPay":0').replace(/\"songVirtualPrice":\d+/g, '\"songVirtualPrice":0').replace(/\"songNeedPay":\d+/g, '\"songNeedPay":0');}
+
+if ($request.url.indexOf(ytapi) != -1){body = $response.body.replace(/\"vipTime":"(.*?)"/g,'\"vipTime":"2099-09-09"');}
+
+$done({body});
