72a79649de5cbf68206fade2c5f844407cd2ccbd
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2022-11-19 21:38:13 +0800

    Update xichuangzhu.js

--- xichuangzhu/Script/xichuangzhu.js
+++ xichuangzhu/Script/xichuangzhu.js
@@ -1,10 +1,3 @@
-/* 
- app下载地址：商店搜索：西窗烛 时间 ：2022-08-08 
- ^https?:\/\/lchttpapi.xczim.com\/1.1\/(users|functions/checkDidBoughtBook|functions/receiveBookCollectionByProUser|functions/getMyCoins|functions/buyFont) url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/xichuangzhu/Script/xichuangzhu.js
- MITM = *.xczim.com
- 作者：清清情 
- */ 
-
  var body = $response.body; 
  var url = $request.url; 
  var obj = JSON.parse(body); 
