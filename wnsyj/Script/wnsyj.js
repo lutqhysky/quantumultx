3057a13ba63431010da6d96917d6688bb01aff7c
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-01-18 09:18:11 +0800

    Create wnsyj.js

---
+++ wnsyj/Script/wnsyj.js
@@ -0,0 +1,13 @@
+var body = $response.body; 
+ var url = $request.url; 
+ var obj = JSON.parse(body); 
+  const buy = 'api/user/profile'; 
+if (url.indexOf(buy) != -1) { 
+    obj.data.__v = 1;
+    obj.data.ispremium = true;
+    obj.data.ispremiumexpired = false;
+    obj.data.premiumexpiretime = "2099-01-18T01:10:10.736Z";
+    obj.data.haspremiumhistory = true;
+ body = JSON.stringify(obj);  
+ } 
+$done({body}); 
