338c6400f0d0edcb84c79fcf50bb29494ee53053
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-01-18 09:28:56 +0800

    Update wnsyj.js

--- wnsyj/Script/wnsyj.js
+++ wnsyj/Script/wnsyj.js
@@ -1,13 +1,18 @@
 var body = $response.body; 
- var url = $request.url; 
- var obj = JSON.parse(body); 
-  const buy = 'api/user/profile'; 
+var url = $request.url; 
+var obj = JSON.parse(body); 
+const buy = 'api/user/profile';
+const buy = 'api/notifications';
 if (url.indexOf(buy) != -1) { 
     obj.data.__v = 1;
     obj.data.ispremium = true;
     obj.data.ispremiumexpired = false;
     obj.data.premiumexpiretime = "2099-01-18T01:10:10.736Z";
     obj.data.haspremiumhistory = true;
+ body = JSON.stringify(obj);  
+ } 
+if (url.indexOf(buy) != -1) { 
+    obj.data.__v = 1;
  body = JSON.stringify(obj);  
  } 
 $done({body}); 
