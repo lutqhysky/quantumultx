a2d7c491a858a695001a4a24cb690056b0464d7f
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-01-29 09:32:05 +0800

    Update wnsyj.js

--- wnsyj/Script/wnsyj.js
+++ wnsyj/Script/wnsyj.js
@@ -1,18 +1,24 @@
 var body = $response.body; 
 var url = $request.url; 
 var obj = JSON.parse(body); 
-const buy = 'api/user/profile';
-const buy = 'api/notifications';
-if (url.indexOf(buy) != -1) { 
+const Vipdate = 'api/user/profile';
+const vip = 'api/notifications';
+if (url.indexOf(Vipdate) != -1) { 
     obj.data.__v = 1;
     obj.data.ispremium = true;
     obj.data.ispremiumexpired = false;
-    obj.data.premiumexpiretime = "2099-01-18T01:10:10.736Z";
+    obj.data.premiumexpiretime = "2099-12-12T01:10:10.736Z";
     obj.data.haspremiumhistory = true;
  body = JSON.stringify(obj);  
  } 
-if (url.indexOf(buy) != -1) { 
+if (url.indexOf(vip) != -1) { 
     obj.data.__v = 1;
  body = JSON.stringify(obj);  
- } 
+ }
+
+if ($request.url.indexOf(getway) != -1){
+    body = $response.body.replace(/\"__v":\d+/g, '\"__v":1');
+}
+
+
 $done({body}); 
