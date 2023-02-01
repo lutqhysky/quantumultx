5b777a9e64b0d3a93c5124f04f08280e6bd183ca
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-02-01 09:57:58 +0800

    Update vipTime.js

--- yunting/Scri/vipTime.js
+++ yunting/Scri/vipTime.js
@@ -1,3 +1,3 @@
 const ytapi = "ytapi.radio.cn/ytsrv/srv/appUser/getUserInfoH5";
-if ($request.url.indexOf(ytapi) != -1){body = $response.body.replace(/\"vipTime":"(.*?)"/g,'\"vipTime":"2099-10-23"');}
+if ($request.url.indexOf(ytapi) != -1){body = $response.body.replace(/\"vipTime":"(.*?)"/g,'\"vipTime":"2099-10-23"').replace(/\"isVip":\d+/g,'\"isVip":1');}
 $done({body})
