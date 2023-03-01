b471e11e5f8913ca3eec4c6e5a62d2a9752eddb5
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-03-01 20:28:52 +0800

    Update mindmaster.js

--- mindmaster/Script/mindmaster.js
+++ mindmaster/Script/mindmaster.js
@@ -1,2 +1,2 @@
-var body = $response.body.replace(/is_vip":\d+/g, 'is_vip":1').replace(/mobile_exp":".*?"/g, 'mobile_exp":"2099-09-01 16:00:26"');
+var body = $response.body.replace(/master_vip":\d+/g, 'master_vip":1').replace(/mobile_exp":".*?"/g, 'mobile_exp":"2099-09-01 16:00:26"');
 $done({ body });
