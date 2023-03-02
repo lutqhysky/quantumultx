50f80e8fc040b286682bb63f2bf75e0235b452b0
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-03-02 15:37:29 +0800

    Update mindmaster.js

--- mindmaster/Script/mindmaster.js
+++ mindmaster/Script/mindmaster.js
@@ -1,2 +1,2 @@
-var body = $response.body.replace(/master_vip":\d+/g, 'master_vip":1').replace(/mobile_exp":".*?"/g, 'mobile_exp":"2099-09-01 16:00:26"').replace(/max_storage":\d+/g, 'max_storage":9073741824');
+var body = $response.body.replace(/exp":".*?"/g, 'exp":"2099-09-01 16:00:26"').replace(/master_vip":\d+/g, 'master_vip":1').replace(/mobile_exp":".*?"/g, 'mobile_exp":"2099-09-01 16:00:26"').replace(/max_storage":\d+/g, 'max_storage":9073741824');
 $done({ body });
