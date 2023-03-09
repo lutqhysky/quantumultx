05efbb121eecd472938936c3f34995917a260152
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-03-09 14:55:03 +0800

    Update zhixi.js

--- zhixi/Script/zhixi.js
+++ zhixi/Script/zhixi.js
@@ -5,9 +5,11 @@ var obj = JSON.parse(body);
 const vip = '/api/ucenter/v1/wechat/login';
 
 if (url.indexOf(vip) != -1) {
-obj.res.token.expire_time = 6678351568;
-obj.res.userinfo.verified = 1; 
-obj.res.userinfo.reg_type = 1;  
-    body = JSON.stringify(obj);    
+    obj.res.token.expire_time = 6678351568;
+    obj.res.userinfo.verified = 1;
+    obj.res.userinfo.reg_type = 1;
+    body = JSON.stringify(obj);
 }
-$done({body});
+$done({
+    body
+});
