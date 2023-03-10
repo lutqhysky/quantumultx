3611c178d688f4898512950b7ebbf134c7dd2487
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-03-10 14:09:24 +0800

    Update zhulong.js

--- zhulong/Script/zhulong.js
+++ zhulong/Script/zhulong.js
@@ -1,12 +1,17 @@
  var body = $response.body; 
  var url = $request.url; 
  var obj = JSON.parse(body); 
-  const buy = '/edu/prod-api/lesson/lesson/getScheduleByLessonId'; 
- const bought = '/ucenter/prod-api/personal/History/lessonview'; 
- const buybuy = '/edu/prod-api/lesson/lesson/getLesson'; 
- const play = '/edu/prod-api/lesson/lesson/play'; 
- const down = '/bbs/prod-api/thread/thread/getThreadForTid'; 
-  if (url.indexOf(buy) != -1) { 
+const vip = '/ucenter/prod-api/personal/myinfo/getUserInfo';  
+const buy = '/edu/prod-api/lesson/lesson/getScheduleByLessonId'; 
+const bought = '/ucenter/prod-api/personal/History/lessonview'; 
+const buybuy = '/edu/prod-api/lesson/lesson/getLesson'; 
+const play = '/edu/prod-api/lesson/lesson/play'; 
+const down = '/bbs/prod-api/thread/thread/getThreadForTid'; 
+ if (url.indexOf(vip) != -1) { 
+ obj.result.is_vip = 1; 
+ body = JSON.stringify(obj);  
+ }   
+if (url.indexOf(buy) != -1) { 
  obj.result.is_buy = 1; 
  body = JSON.stringify(obj);  
  } 
