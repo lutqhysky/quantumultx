cb2ed12cc602e76444dfb40da9f44936d8a9e388
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-03-21 10:45:06 +0800

    Update HIFI.js

--- VIPER HIFI/Script/HIFI.js
+++ VIPER HIFI/Script/HIFI.js
@@ -7,9 +7,15 @@ var url = $request.url;
 var obj = JSON.parse(body);
 
 const vip = '/v1/hifi/membership/vipinfo';
+const vip = 'v1/membership/vipuser';
 if (url.indexOf(vip) != -1) {
     obj.data.end_time = "2099-12-12 10:23:49";
     obj.data.have_paid = true;
+    obj.data.is_vip = 1;
+    body = JSON.stringify(obj);
+}
+if (url.indexOf(vip) != -1) {
+    obj.data.end_time = "2099-12-12 10:23:49";
     obj.data.is_vip = 1;
     body = JSON.stringify(obj);
 }
