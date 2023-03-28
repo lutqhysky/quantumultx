d68972495c956c59fcbff8cf0ac2f7bd017de670
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-03-28 13:38:34 +0800

    Create seqpy.js

---
+++ qupeiyin/Script/seqpy.js
@@ -0,0 +1,17 @@
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+const vip = '/member';
+if (url.indexOf(vip) != -1) {
+    obj.data.is_svip = "1";
+    obj.data.is_vip = "1";
+    obj.data.svip_endtime = "1891758867";
+    obj.data.vip_endtime = "1891758867";
+      obj.data.libu_vip_endtime = "1891758867";
+    body = JSON.stringify(obj);
+
+}
+
+$done({
+    body
+});
