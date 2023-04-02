69edee32c632e293f252f1e27f2f245e2e3a34c0
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-04-02 15:59:12 +0800

    Update anruankoutu.js

--- aoruankoutu/Script/anruankoutu.js
+++ aoruankoutu/Script/anruankoutu.js
@@ -25,28 +25,23 @@ hostname=gw.aoscdn.com
 
 var cuttlefish ={"warning":"本腳本僅供學習交流使用，禁止轉載售賣","tgchannel":"https://t.me/ddgksf2021","feedback":"https://t.me/ddgksf2013_bot"};
 var ddgksf2013 = {
-  "status" : 200,
-  "message" : "success",
-  "data" : {
-    "expired_at" : 4100748696,
-    "is_activated" : 1,
-    "is_lifetime" : 1,
-    "expire_time" : "2099-12-12 00:00:00",
-    "candy" : 1,
-    "device_id" : 600210469,
-    "exist_trial" : 1,
-    "period_type" : "active",
-    "remain_days" : 9999,
-    "candy_expired_at" : 4100748696,
-    "product_id" : 369,
-    "has_present" : 1,
-    "allowed_device_count" : 999999,
-    "has_buy_extend" : 0,
-    "will_expire" : 0,
-    "license_type" : "premium",
-    "begin_activated_time" : 1647732362,
-    "durations" : 1,
-    "vip_special" : 1
+  "status": 200,
+  "message": "success",
+  "data": {
+    "group_expired_at": 4100748696,
+    "is_tried": 0,
+    "max_devices": 1000,
+    "period_type": "premium",
+    "candy_expired_at": 4100748696,
+    "pending": 0,
+    "remained_seconds": 0,
+    "limit": 0,
+    "expired_at": 4100748696,
+    "candy": 1,
+    "license_type": "premium",
+    "quota": 0,
+    "status": 0,
+    "coin": 9999
   }
 };
 $done({body: JSON.stringify(ddgksf2013)});
