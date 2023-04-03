abfe992c461ffcb1c5b6d03d14a39ced6aba2893
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-04-03 10:11:57 +0800

    Update HiFi.js

--- VIPERHIFI/Script/HiFi.js
+++ VIPERHIFI/Script/HiFi.js
@@ -1,14 +1,23 @@
 /*
 ^https?:\/\/(viperhifi|media\.store)\.kugou\.com\/v1\/(hifi\/membership\/vipinfo|membership\/vipuser|get_res_privilege)
+http://login.user.kugou.com/v5/login_by_token
 */
 
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
-
+const svip = '/v5/login_by_token';
 const vip = '/v1/hifi/membership/vipinfo';
 const Vip = 'v1/membership/vipuser';
 const song = 'v1/get_res_privilege';
+
+if (url.indexOf(svip) != -1) {
+    obj.data.su_vip_end_time = "2099-12-12 10:23:49";
+    obj.data.vip_end_time = "2099-12-12 10:23:49";
+    obj.data.vip_type = 3;
+    obj.data.is_vip = 1;
+    body = JSON.stringify(obj);
+}
 if (url.indexOf(vip) != -1) {
     obj.data.end_time = "2099-12-12 10:23:49";
     obj.data.have_paid = true;
