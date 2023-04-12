32ffe3425d7d8666d9d5c7596049702956d6f680
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-04-12 20:58:07 +0800

    Update slzd.js

--- sanlianzhongdu/Script/slzd.js
+++ sanlianzhongdu/Script/slzd.js
@@ -7,11 +7,11 @@ const zhuanlan = "/zhuanlan/zhuanlanV50305.do";
 let obj = JSON.parse($response.body);
 
  if (url.indexOf(zhuanlan) != -1) {
- body = $response.body.replace(/"isSubscribed":\w+/g, "\"isSubscribed\":true")
-                      .replace(/"vipe":\w+/g, "\"vip\":true")
-                      .replace(/"vipValid":\w+/g, "\"vipValid\":true")
-                      .replace(/"subscribeState":\d+/g, "\"subscribeState\":1")
-                      .replace(/"feeType":\d+/g, "\"feeType\":0")
+ body = $response.body.replace(/"isSubscribed":\w+/g, '\"isSubscribed\":true')
+                      .replace(/"vipe":\w+/g, '\"vip\":true')
+                      .replace(/"vipValid":\w+/g, '\"vipValid\":true')
+                      .replace(/"subscribeState":\d+/g, '\"subscribeState\":1')
+                      .replace(/"feeType":\d+/g, '\"feeType\":0')
 $done({body});
  }
 $done({body:JSON.stringify(obj)});
