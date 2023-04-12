7033474a811f78a7ca397bffbb63b60e2eb3e327
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-04-12 23:02:21 +0800

    Update slzd.js

--- sanlianzhongdu/Script/slzd.js
+++ sanlianzhongdu/Script/slzd.js
@@ -4,8 +4,9 @@
 */ 
 
 
-const zhuanlan = "(/zhuanlan/zhuanlanV50305.do|/article/detailV50305)";
+const zhuanlan = "/zhuanlan/zhuanlanV50305.do";
 const bqc = "/api/appDistribution/distributionPoster";
+const vip = "/article/detailV50305";
 let obj = JSON.parse($response.body);
 
  if ($request.url.indexOf(zhuanlan) != -1) {
@@ -21,6 +22,17 @@ $done({body});
  }
 if ($request.url.indexOf(bqc) != -1) {
 obj.model.userVflag = 1
+ }
+if ($request.url.indexOf(vip) != -1) {
+ body = $response.body.replace(/isSubscribed\":\w+/g, 'isSubscribed":true')
+                      .replace(/vip\":\w+/g, 'vip":true')
+                      .replace(/vipValid\":\w+/g, 'vipValid":true')
+                      .replace(/subscribeState\":\d+/g, 'subscribeState":1')
+                      .replace(/feeType\":\d+/g, 'feeType":0')
+                      .replace(/type\":\d+/g, 'type":4')
+                      .replace(/zhunalanSubscribe\":\w+/g, 'zhunalanSubscribe":true')
+                      .replace(/vipExpiration\":".*?"/g, 'vipExpiration":"2099-12-12"')
+$done({body});
  }
 $done({body:JSON.stringify(obj)});
 
