46a06e858cb425b5e331dbabb9b2904a6d5c2616
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-04-12 22:47:44 +0800

    Update slzd.js

--- sanlianzhongdu/Script/slzd.js
+++ sanlianzhongdu/Script/slzd.js
@@ -10,7 +10,7 @@ let obj = JSON.parse($response.body);
 
  if ($request.url.indexOf(zhuanlan) != -1) {
  body = $response.body.replace(/isSubscribed\":\w+/g, 'isSubscribed":true')
-                      .replace(/vipe\":\w+/g, 'vip":true')
+                      .replace(/vip\":\w+/g, 'vip":true')
                       .replace(/vipValid\":\w+/g, 'vipValid":true')
                       .replace(/subscribeState\":\d+/g, 'subscribeState":1')
                       .replace(/feeType\":\d+/g, 'feeType":0')
