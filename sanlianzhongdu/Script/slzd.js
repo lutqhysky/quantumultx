59eb2b064c43e87564cfc17af32085a4fa2d1f35
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-04-13 10:25:12 +0800

    Update slzd.js

--- sanlianzhongdu/Script/slzd.js
+++ sanlianzhongdu/Script/slzd.js
@@ -7,6 +7,7 @@
 const zhuanlan = "/zhuanlan/zhuanlanV50305.do";
 const bqc = "/api/appDistribution/distributionPoster";
 const vip = "/article/detailV50305";
+const ceshi = "/article/detailV50305";
 let obj = JSON.parse($response.body);
 
  if ($request.url.indexOf(zhuanlan) != -1) {
@@ -33,6 +34,9 @@ if ($request.url.indexOf(vip) != -1) {
                       .replace(/zhunalanSubscribe\":\w+/g, 'zhunalanSubscribe":true')
                       .replace(/vipExpiration\":".*?"/g, 'vipExpiration":"2099-12-12"')
 $done({body});
+ }
+if ($request.url.indexOf(ceshi) != -1) {
+obj.model.data = 1
  }
 $done({body:JSON.stringify(obj)});
 
