eb6b2747e16fc022a8c80c1883a82d722d7f2b22
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-04-26 13:27:43 +0800

    Update Libvio.js

--- Libvio/Script/Libvio.js
+++ Libvio/Script/Libvio.js
@@ -8,6 +8,8 @@ var url = $request.url;
 var obj = JSON.parse(body);
 
 const vip = '/xgapp.php/v3/video_detail';
+const path2 = '/xgapp.php/v3/video';
+
 if (url.indexOf(vip) != -1) {
 obj.data.permission_info.download_permission = 1;
 obj.data.permission_info.trysee_permission = 1;
@@ -16,7 +18,7 @@ obj.data.permission_info.play_need_permission = 0;
 obj.data.permission_info.play_permission = 1;    
     body = JSON.stringify(obj);    
 }
-if (url.indexOf(vip) != -1) {
+if (url.indexOf(path2) != -1) {
 obj. no_permission = flase;  
     body = JSON.stringify(obj);    
 }
