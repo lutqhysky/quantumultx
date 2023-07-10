369adbab6a5e3fbe28f803ebf7d3bdfb4e17076d
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-07-10 16:40:02 +0800

    Update judu.js

--- judu/Script/judu.js
+++ judu/Script/judu.js
@@ -1,23 +1,12 @@
-/*
-app下载地址：商店搜索：句读
-^https?:\/\/judouapp\.com\/api\/v2\/mine\/profile url script-response-body zhulong.js
-MITM = judouapp.com
-作者：清清情
-*/
-
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
-
 const vip = '/api/v2/mine/profile';
-
-
 if (url.indexOf(vip) != -1) {
     obj.is_member = true;
     obj.is_admin = true;
     obj.is_year_member = true;
-    obj.member_expired_at = 9876543210;
+    obj.member_expired_at = 4100747967;
     body = JSON.stringify(obj);
-    
 }
 $done({body});
