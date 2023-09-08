25fc8c4ace5d2b39563500f4bc504cb9b8735b02
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-09-08 11:07:42 +0800

    更新 xinyutingshu.js

--- xinyutingshu/Script/xinyutingshu.js
+++ xinyutingshu/Script/xinyutingshu.js
@@ -1,10 +1,3 @@
-/*
-app下载地址：心语听书
-^https?:\/\/i\.xinyulib\.com\.cn\/api\/querytoken url script-response-body https://raw.githubusercontent.com/lutqhysky/quantumultx/mylove/xinyutingshu/Script/xinyutingshu.js
-MITM =i.xinyulib.com.cn
-作者：清清情
-*/
-
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
@@ -12,7 +5,7 @@ var obj = JSON.parse(body);
 const vip = '/api/querytoken';
 
 if (url.indexOf(vip) != -1) {
-    obj.data.vipendtime = "2029-05-31";   
+    obj.data.vipendtime = "2029-12-12";   
     body = JSON.stringify(obj);    
 }
 $done({body});
