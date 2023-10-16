d998828f1dce009f76ed38f00960da7a49769736
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-10-16 14:27:10 +0800

    Update Libvio.js

--- Libvio/Script/Libvio.js
+++ Libvio/Script/Libvio.js
@@ -1,24 +1,14 @@
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-
-const vip = '/xgapp.php/v3/video_detail';
+const path1 = '/xgapp.php/v3/video_detail';
 const path2 = '/xgapp.php/v3/video';
-
-if (url.indexOf(vip) != -1) {
-obj.data.permission_info.download_permission = 1;
-obj.data.permission_info.trysee_permission = 1;
-obj.data.permission_info.download_need_permission = 0;
-obj.data.permission_info.play_need_permission = 0;
-obj.data.permission_info.play_permission = 1; 
-   body = JSON.stringify(obj);    
+if ($request.url.indexOf(ytapi) != -1) {
+    body = $response.body.replace(/\"download_permission":\d+/g, '\"download_permission":1')
+                         .replace(/\"trysee_permission":\d+/g, '\"trysee_permission":1')
+                         .replace(/\"download_need_permissio":\d+/g, '\"download_need_permissio":1')
+                         .replace(/\"play_need_permission":\d+/g, '\"play_need_permission":1')     
+                         .replace(/\"play_permission":\d+/g, '\"play_permission":1')      
+                         .replace(/\"parse_after_config_enable":\d+/g, '\"parse_after_config_enable":1')    
 }
 if (url.indexOf(path2) != -1) {
-obj.no_permission = false;  
-    body = JSON.stringify(obj);    
+   body = $response.body.replace(/\"no_permission":\w+/g, '\"no_permission":false')
 }
 $done({body});
-
-const path1 = '/xgapp.php/v3/video_detail';
-if ($request.url.indexOf(ytapi) != -1){body = $response.body.replace(/\"download_permission":\d+/g,'\"download_permission":1').replace(/\"trysee_permission":\d+/g,'\"trysee_permission":1');}
-$done({body})
