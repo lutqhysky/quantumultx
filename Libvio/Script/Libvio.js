8447a5923d7a2f69ea11c2e289a3a1e40d68120c
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-10-16 14:49:24 +0800

    Update Libvio.js

--- Libvio/Script/Libvio.js
+++ Libvio/Script/Libvio.js
@@ -3,11 +3,7 @@ const path1 = '/xgapp.php/v3/video_detail';
 const path2 = '/xgapp.php/v3/video';
 let obj = JSON.parse($response.body);
 
-if ($request.url.indexOf(path2) != -1) {
-body = $response.body.replace(/\"no_permission":\w+/g, '\"no_permission":false')
- $done({body});
- }
- if ($request.url.indexOf(path1) != -1) {
+if ($request.url.indexOf(path1) != -1) {
   body = $response.body.replace(/\"download_permission":\d+/g, '\"download_permission":1')
                          .replace(/\"trysee_permission":\d+/g, '\"trysee_permission":1')
                          .replace(/\"download_need_permissio":\d+/g, '\"download_need_permissio":1')
@@ -15,6 +11,11 @@ body = $response.body.replace(/\"no_permission":\w+/g, '\"no_permission":false')
                          .replace(/\"play_permission":\d+/g, '\"play_permission":1')      
                          .replace(/\"parse_after_config_enable":\d+/g, '\"parse_after_config_enable":1') 
 $done({body});
+ }
+
+if ($request.url.indexOf(path2) != -1) {
+body = $response.body.replace(/\"no_permission":\w+/g, '\"no_permission":false')
+ $done({body});
  }
 $done({body:JSON.stringify(obj)});
 
