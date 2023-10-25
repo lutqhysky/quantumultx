c78baf155f5fab3f4b33cce183ffde33e8f816e8
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-10-25 20:33:58 +0800

    更新 Rss.js

--- rss/Script/Rss.js
+++ rss/Script/Rss.js
@@ -3,7 +3,7 @@ var sub = {
     "username": "lutqhysky@outlook.com",
     "membershipType": "subscription",
     "expireAt": 1669351050571,
-    "serverTime": 1666672709136
+    "serverTime": 1666672709136,
     "secret":"1990781058550bcc1265576b54f9c04ebf48a0dfe9557fa849050d4cca81b3b4"
 }
 obj.data = sub;
