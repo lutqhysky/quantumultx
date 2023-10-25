842ff147301fc198a699e0046d20e5b192f11270
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-10-25 20:30:34 +0800

    更新 Rss.js

--- rss/Script/Rss.js
+++ rss/Script/Rss.js
@@ -4,6 +4,7 @@ var sub = {
     "membershipType": "subscription",
     "expireAt": 1669351050571,
     "serverTime": 1666672709136
+    "secret":"1990781058550bcc1265576b54f9c04ebf48a0dfe9557fa849050d4cca81b3b4"
 }
 obj.data = sub;
 obj.data.expireAt = 4100731932571
