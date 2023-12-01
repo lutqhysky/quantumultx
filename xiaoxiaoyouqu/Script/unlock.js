356b601da494eab858d6c1d2625a5f7db79cc0ec
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2023-12-01 16:43:38 +0800

    Delete xiaoxiaoyouqu/Script/unlock.js

--- xiaoxiaoyouqu/Script/unlock.js
+++
@@ -1,8 +0,0 @@
-/* 
-^https?:\/\/fastapi\.ukids\.cn\/coreapp\/classOqd\/course\/detail
- */ 
-
-
-
-body = $response.body.replace(/unlock":false/g, 'unlock":true').replace(/purchased":false/g, 'subscribable":true');
-$done({body});
