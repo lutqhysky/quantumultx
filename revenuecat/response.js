62cca41c8f04b947cd6010677c8cd331dd56e45e
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2024-01-06 14:30:34 +0800

    更新 response.js

--- revenuecat/response.js
+++ revenuecat/response.js
@@ -158,8 +158,8 @@ if (typeof $response == "undefined") {
     'reader':{ name: 'vip2', id: 'com.valo.reader.vip2.year'},
     '%E4%BA%8B%E7%BA%BF':{ name: 'pro', id: 'xyz.jiaolong.eventline.pro.lifetime'},
     'dtdvibe':{ name: 'pro', id: 'com.dtd.aroundu.year'},
-    '%E8%B0%9C%E5%BA%95%E6%97%B6%E9%92%9F':{ name: 'pro', id: 'tech.miidii.MDClock.subscription.year'},
-    'Cadmio':{ name: 'editor', id: 'cd_1y_2w0'},
+    //'%E8%B0%9C%E5%BA%95%E6%97%B6%E9%92%9F':{ name: 'pro', id: 'tech.miidii.MDClock.subscription.year'},
+    //'Cadmio':{ name: 'editor', id: 'cd_1y_2w0'},
     
   };
   const data = {
