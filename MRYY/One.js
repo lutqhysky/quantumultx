d415efa6c45b8542a76ae148e2cafc6151275203
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2024-04-10 23:25:24 +0800

    更新 One.js

---
+++ MRYY/One.js
@@ -0,0 +1,83 @@
+const $tool = new Tool();
+$tool.get('https://dict.youdao.com/infoline/style/cardList?mode=publish&client=mobile&style=daily&size=2', function (error, response, data) {
+    let obj = JSON.parse(data);
+    let date = new Date();
+    let isAM = date.getHours() < 12;
+    let title = '每日' + (isAM ? '一言' : '一言') + (isAM ? ' ☀️' : ' 🌙');
+    let subtitle = '';
+    let content = obj[1] ? obj[1].title + '\n' + obj[1].summary : '';
+
+    if (!error && obj && obj.length > 1) {
+        let yi = obj[1];
+        let imageUrl = yi.image[0];  
+        let options = {
+            "action": "clipboard",
+            "text": content,  
+            "media-url": imageUrl,  
+            "sound": true  
+        };
+        $tool.notify(title, subtitle, content, options);
+    } else {
+
+        $tool.notify(title, subtitle, content);
+    }
+    $done();
+});
+
+
+function Tool() {
+    _node = (() => {
+        if (typeof require == "function") {
+            const request = require('request')
+            return ({ request })
+        } else {
+            return (null)
+        }
+    })()
+    _isSurge = typeof $httpClient != "undefined"
+    _isQuanX = typeof $task != "undefined"
+    this.isSurge = _isSurge
+    this.isQuanX = _isQuanX
+    this.isResponse = typeof $response != "undefined"
+    this.notify = (title, subtitle, message, option) => {
+        if (_isQuanX) $notify(title, subtitle, message, option)
+        if (_isSurge) $notification.post(title, subtitle, message, option)
+        if (_node) console.log(JSON.stringify({ title, subtitle, message }));
+    }
+    this.write = (value, key) => {
+        if (_isQuanX) return $prefs.setValueForKey(value, key)
+        if (_isSurge) return $persistentStore.write(value, key)
+    }
+    this.read = (key) => {
+        if (_isQuanX) return $prefs.valueForKey(key)
+        if (_isSurge) return $persistentStore.read(key)
+    }
+    this.get = (options, callback) => {
+        if (_isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "GET"
+            $task.fetch(options).then(response => { callback(null, _status(response), response.body) }, reason => callback(reason.error, null, null))
+        }
+        if (_isSurge) $httpClient.get(options, (error, response, body) => { callback(error, _status(response), body) })
+        if (_node) _node.request(options, (error, response, body) => { callback(error, _status(response), body) })
+    }
+    this.post = (options, callback) => {
+        if (_isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "POST"
+            $task.fetch(options).then(response => { callback(null, _status(response), response.body) }, reason => callback(reason.error, null, null))
+        }
+        if (_isSurge) $httpClient.post(options, (error, response, body) => { callback(error, _status(response), body) })
+        if (_node) _node.request.post(options, (error, response, body) => { callback(error, _status(response), body) })
+    }
+    _status = (response) => {
+        if (response) {
+            if (response.status) {
+                response["statusCode"] = response.status
+            } else if (response.statusCode) {
+                response["status"] = response.statusCode
+            }
+        }
+        return response
+    }
+}
