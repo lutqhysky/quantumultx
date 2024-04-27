211feedb88a2b656cc796e08524aef6d712b0cea
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2024-04-27 10:02:52 +0800

    更新 jianxun.js

--- jianxu/Script/jianxun.js
+++ jianxu/Script/jianxun.js
@@ -2,5 +2,5 @@ let obj = JSON.parse($response.body);
 obj["data"]["is_vip"] = true,
 obj["data"]["vip_expire_time"] = "2099-12-12 00:00:00",
 obj["data"]["is_broadcast"] = 1,
-obj["data"]["is_bc"] = 1,
+obj["data"]["is_broadcast"] = 0,
 $done({body: JSON.stringify(obj)});
