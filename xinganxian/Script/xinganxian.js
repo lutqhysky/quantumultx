e6312b78e131ccdc126901b734833268f006e29f
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2024-05-12 20:54:36 +0800

    更新 xinganxian.js

--- xinganxian/Script/xinganxian.js
+++ xinganxian/Script/xinganxian.js
@@ -1,9 +1,4 @@
-/* 
- app下载地址：商店搜索：新干线 时间 ：2022-08-16 
- ^https?:\/\/api\.fwxgx\.com\/aggregation\/v1\/courses url script-response-body zhulong.js 
- MITM = api.fwxgx.com
- 作者：清清情 
- */ 
+
  var body = $response.body; 
  var url = $request.url; 
  var obj = JSON.parse(body); 
