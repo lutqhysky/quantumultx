39dfa37138e1fd738a929b33bd5200a5e7370185
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2025-02-21 09:38:42 +0800

    Delete JAVDB/Script directory

--- JAVDB/Script/javdb.js
+++
@@ -1,7 +0,0 @@
-var body = $response.body;
-var lutqhysky = JSON.parse(body);
-const ada = '/api/v1/ads';
-if ($request.url.indexOf(ada) != -1){
-  lutqhysky.data.ads = {};
-}
-$done({body : JSON.stringify(lutqhysky)});
