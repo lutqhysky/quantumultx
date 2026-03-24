b0488d9e3f5d6105528a6ef7e92d5a9336848055
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2026-03-24 13:43:59 +0800

    Update response.js

--- revenuecat/response.js
+++ revenuecat/response.js
@@ -64,6 +64,7 @@ if (typeof $response === "undefined") {
                 'Leica%20LUX': { name: 'pro', id: 'annual_subscribers_first_cohort' },
                 'SleepSounds': { name: 'vip', id: 'VIPOneMonth' },
                 'iplayTV': { name: 'com.ll.btplayer.12', id: 'com.ll.btplayer.12' },
+                'WaterMinder': { name: 'waterminder-pro', id: 'waterminder.premiumYearly' },
                  'Endel': { name: 'pro', id: 'Lifetime'},  //Endel
                 'Gentler': { name: 'premium', id: 'app.gentler.activity.nonconsumable.onetime1' },
                 'Law': { name: 'vip', id: 'LawVIPOneYear'},  //中国法律
