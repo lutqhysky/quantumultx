fc62d6419418348f8feff150dcd60fe5bb2af1f8
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2026-03-26 21:44:51 +0800

    更新 response.js

--- revenuecat/response.js
+++ revenuecat/response.js
@@ -1,6 +1,6 @@
 const $ = new Env("RevenueCatUltraHybrid");
 const NOTIFY_INTERVAL_HOURS = 2; 
-const EXCLUDE_APPS = ['ReflixiOS', 'Fileball', 'APTV', 'Forward']; 
+const EXCLUDE_APPS = ['Authenticator','ReflixiOS', 'Fileball', 'APTV', 'Forward']; 
 
 if (typeof $response === "undefined") {
     // --- 1. 请求阶段：移除缓存指纹 ---
