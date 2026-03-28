015da66e0905455b108633a5c38bfcfdac414ae7
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2026-03-28 22:58:28 +0800

    更新 response.js

--- revenuecat/response.js
+++ revenuecat/response.js
@@ -52,6 +52,7 @@ if (typeof $response === "undefined") {
             };
 
             const UAMappings = {
+                'Bookfy': { name: 'Bookfy Pro', id: 'bookfy_lifetime' },
                 'Charger': { name: 'pro', id: 'com.charginglite.weekly' },
                 'Halo': { name: 'premium', id: 'halo_5999_1y_1w0' },
                 'Structured': { name: 'pro', id: 'today.structured.pro' },
