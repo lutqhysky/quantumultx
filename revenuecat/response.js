89bdfbbcc15ca23ee7d06c0bd20866acfc74d181
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2026-04-04 14:31:52 +0800

    更新 response.js

--- revenuecat/response.js
+++ revenuecat/response.js
@@ -52,6 +52,7 @@ if (typeof $response === "undefined") {
             };
 
             const UAMappings = {
+                'AdGuard%20Home%20Remote': { name: 'aghrpro', id: 'adguard.home.remote.pro' },
                 'Pillow': { name: 'premium', id: 'com.neybox.pillow.premium.year' },
                 'Bookfy': { name: 'Bookfy Pro', id: 'bookfy_lifetime' },
                 'Charger': { name: 'pro', id: 'com.charginglite.weekly' },
